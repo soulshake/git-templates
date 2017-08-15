832031aeb04292e0523120313cb5e0298094cdb8
AJ Bowen <aj@soulshake.net>
2017-08-15 10:51:15 +0200

    Add travis lint pre-commit hook

---
+++ hooks/pre-commit.d/00-travis-lint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+travis_yml_path="$(git rev-parse --show-toplevel)/.travis.yml"
+
+if [ ! -f "$travis_yml_path" ]; then
+    echo "$travis_yml_path not found; skipping pre-commit hook 'travis lint'"
+    exit 0
+fi
+
+travis_path="$(/bin/which travis)"
+if [ ! -x "$travis_path" ]; then
+    echo "travis CLI not installed or not in PATH; skipping pre-commit hook 'travis lint'"
+    exit 0
+fi
+
+echo travis lint --exit-code "$travis_yml_path"
