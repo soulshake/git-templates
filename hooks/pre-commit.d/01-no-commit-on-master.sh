af11c0bff1ba156a0ff20d920575fa4490134a95
AJ Bowen <aj@soulshake.net>
2018-11-16 09:59:15 -0500

    allow commit to master if there's no remote

--- hooks/pre-commit.d/01-no-commit-on-master.sh
+++ hooks/pre-commit.d/01-no-commit-on-master.sh
@@ -11,8 +11,11 @@ set -e
 result=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$result" == "master" ]]; then 
-    # Only prevent committing on master if not on our own repo
-    if [[ ! $(git config remote.origin.url) == *$(git config github.user)* ]]; then
+    # Only prevent committing on master if not on our own repo or if there is no remote
+    remote_url="$(git config remote.origin.url)"
+    if [[ "$remote_url" == *$(git config github.user)* ]] || [[ "$remote_url" == "" ]]; then
+      echo "ok to commit"
+    else
       echo "Cannot commit on master (bypass with -n or --no-verify)"
       exit 1
     fi
