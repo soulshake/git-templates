cb70de149ec026c22638758cc349dce5d3273dba
AJ Bowen <aj@soulshake.net>
2019-10-03 16:55:49 +0200

    Remove whitespace

--- hooks/pre-commit.d/01-no-commit-on-master.sh
+++ hooks/pre-commit.d/01-no-commit-on-master.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # need to break this out from the if statement, otherwise set -e won't cause an exit
-result=$(git rev-parse --abbrev-ref HEAD &> /dev/null) 
+result=$(git rev-parse --abbrev-ref HEAD &> /dev/null)
 if [ $? -eq 128 ]; then
     echo "No commits yet"
     exit 0
 fi
 
-set -e
 result=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ "$result" == "master" ]]; then 
+if [[ "$result" == "master" ]]; then
     # Only prevent committing on master if not on our own repo or if there is no remote
     remote_url="$(git config remote.origin.url)"
     if [[ "$remote_url" == *$(git config github.user)* ]] || [[ "$remote_url" == "" ]]; then
